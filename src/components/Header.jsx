c3b3a497a9739e6e45155858c623ed44dece6dd1
somesh-dahatonde <sddahatonde22@gmail.com>
2022-06-26 01:24:37 +0530

    landing page created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,33 @@
 import React from "react";
-import './header.css'
+import "./header.css";
 
-function Header (props) {
-
-    return (
-        <>
-        <nav className="nav">
-            <div className="logo-div">
-                <img src='../asset/Logo.png' alt="image not rendered"/>
-            </div>
-            <div className="sign-indiv">
-                <ul>
-                    <li>
-                    
-                        <select>
-                            <span>sd</span>
-                            <option>English</option>
-                            <option>Marathi</option>
-                        </select>
-                    </li>
-                    <li>
-                        <a href={props.src}>
-                        <button src={props.src}>{props.name}</button>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-        </>
-    )
+function Header(props) {
+  return (
+    <>
+      <nav className="nav">
+        <div className="logo-div">
+          {/* // eslint-disable-next-line */}
+          <img src="../asset/Logo.png" alt="image not rendered" />
+        </div>
+        <div className="sign-indiv">
+          <ul>
+            <li>
+              <select>
+                <span>sd</span>
+                <option>English</option>
+                <option>Marathi</option>
+              </select>
+            </li>
+            <li>
+              <a href={props.src}>
+                <button src={props.src}>{props.name}</button>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
 }
-
 
 export default Header;
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,25 +1,50 @@
 import React from "react";
 import Header from "./Header";
-import './MainPage.css';
+import "./MainPage.css";
 
-const MainPage =() => {
-    return (
-        <>
-        <div className="main-div">
-        
-            <Header name = "Sign In "/>
-            <div className="Center-content">
-            <img src='../asset/image1.png' alt="image not rendered"/>
-            <h1>Unlimited movies, TV shows and more.</h1>
-            <h3>Watch anywhere. Cancel anytime.</h3>
-            <h6>Ready to watch? Enter your email to create or restart your membership.
-</h6>
-<input type='email' />
-<button className="get-stated-button">Get Started</button>
-</div>
+const MainPage = () => {
+  return (
+    <>
+      <div className="main-div">
+        <Header name="Sign In " />
+
+        <img
+          className="background_img"
+          src="../asset/image1.png"
+          alt="image not rendered"
+        />
+        {/* // eslint-disable-next-line */}
+        <div className="Center-content">
+          <h1 className="h1">Unlimited movies, TV shows and more.</h1>
+          <p className="h3">Watch anywhere. Cancel anytime.</p>
+          <p className="h6">
+            Ready to watch? Enter your email to create or restart your
+            membership.
+          </p>
+          <div className="input_div">
+            <input type="email" placeholder="Email address" />
+            <button className="get-stated-button">Get Started</button>
+          </div>
         </div>
-        </>
-    )
-}
+      </div>
+      <div className="tv-div">
+        <div className="tv-content">
+          <h1>Enjoy on your TV.</h1>
+          <p>
+            Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray
+            players and more.
+          </p>
+        </div>
+        <div className="tv-img-div">
+          <img className="img1" src="../asset/Rectangle.png" />
+          <img
+            className="img2"
+            src="../asset/Netflix India – Watch TV Shows Online, Watch Movies Online_2 1.png"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default MainPage;
