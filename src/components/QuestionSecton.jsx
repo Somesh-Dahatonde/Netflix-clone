39b161a77a876a853cd0dc9be3e1afced02635a7
somesh-dahatonde <sddahatonde22@gmail.com>
2022-06-28 23:47:09 +0530

    btnfunctions added

--- src/components/QuestionSecton.jsx
+++ src/components/QuestionSecton.jsx
@@ -5,6 +5,7 @@ const Style = {
   height: "40px",
   outline: "none",
   border: "none",
+  marginBottom: "20px ",
 };
 
 const Question_div = {
@@ -12,14 +13,40 @@ const Question_div = {
   flexDirection: "column",
   alignItem: "center",
   width: "100%",
-  margin: "0 auto",
+  justifyContent: "center",
+  alignItems: "Center",
 };
-
 const QuestionSection = () => {
   return (
     <>
       <div style={Question_div}>
         <h1>Frequently Asked Questions</h1>
+
+        <button
+          onClick={() => {
+            let x = document.getElementById("text").style.display;
+            console.log(x);
+            if (x === "none") {
+              x.style.display = "block";
+              console.log(x);
+            } else {
+              x.style.display = "none";
+              console.log(x);
+            }
+          }}
+          style={{ color: "black" }}
+          value=" What is Netflix ? "
+        >
+          What is Netflix ?
+        </button>
+        <p id="text" style={{ display: "none" }}>
+          Netflix is a streaming service that offers a wide variety of
+          award-winning TV shows, movies, anime, documentaries and more – on
+          thousands of internet-connected devices. You can watch as much as you
+          want, whenever you want, without a single ad – all for one low monthly
+          price. There's always something new to discover, and new TV shows and
+          movies are added every week!
+        </p>
         <select style={Style}>
           <option selected> What is Netflix</option>
 
